9a4ca2e365fff2806a70664016a41a2e5939fcc5
Brunin-S <santanabruno.kg@gmail.com>
2024-03-28 20:31:49 -0300

    API EM PHP

---
+++ record.php
@@ -0,0 +1,39 @@
+<?php
+$url = 'https://www.admin.virtuamegastore.com.br/api/banners2.php';
+$data = file_get_contents($url);
+$data = json_decode($data,true)
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Bruno || API//Carrosel</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+    </script>
+</head>
+<body>
+<div id="carouselExample" class="carousel slide">
+  <div class="carousel-inner">
+    <?php foreach($data as $k => $v) {?>
+    <div class="carousel-item active">
+      <img src="<?=$v['img']?>" class="d-block w-100">
+    </div>
+    <?php
+    }
+    ?>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</body>
+</html>
